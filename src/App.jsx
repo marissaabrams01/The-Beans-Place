9c15293bf8ece774a3416f417005a21203c7eb03
Marissa Abrams <whyphy.abrams@gmail.com>
2026-09-17 18:48:49 -0600

    contactSetion

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,7 @@ import RibbonTicker from "./components/RibbonTicker";
 import FeaturesSection from "./components/FeaturesSection"; 
 import ProductShowcase from "./components/ProductShowcase";
 import AboutSection from "./components/AboutSection";
+import CtaSection from "./components/CtaSection";
 
 // STEP 2: Create and export the App component
 // Use: export default function App() { ... }
@@ -91,7 +92,7 @@ export default function App(){
 
             {/* CTA */}
             <section className = "bg-cta">
-
+            <CtaSection />
 
             </section>
 
